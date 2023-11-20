30b0f1d1c7a477777fecd63263f603c9ad66b2b3
AnayochukwuJames <anayochukwujames11@gmail.com>
2023-11-20 19:55:34 +0100

    Update initial commit

--- src/main/java/com/example/userregistrationapp/controller/UserController.java
+++ src/main/java/com/example/userregistrationapp/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.example.userregistrationapp.controller;
 
+import com.example.userregistrationapp.dto.UpdateResponse;
 import com.example.userregistrationapp.dto.UserRequest;
+import com.example.userregistrationapp.dto.UserResponse;
 import com.example.userregistrationapp.model.User;
 import com.example.userregistrationapp.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -16,5 +18,12 @@ public class UserController {
     public ResponseEntity<User> createUser(@RequestBody UserRequest request) {
         return userService.createUser(request);
     }
-
+@GetMapping("get-user")
+    public ResponseEntity<UserResponse> getUserById(@PathVariable Long id){
+        return userService.getUserById(id);
+}
+@PutMapping("user-update")
+public ResponseEntity<UserResponse> updateUser(@PathVariable Long id, @RequestBody UpdateResponse updateResponse){
+        return userService.updateUser(id, updateResponse);
+}
 }
