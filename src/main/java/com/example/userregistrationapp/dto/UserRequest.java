861f28c5c06d28c7956bf6fef223bbd520fce060
AnayochukwuJames <anayochukwujames11@gmail.com>
2023-11-23 22:57:31 +0100

    Update initial commit

--- src/main/java/com/example/userregistrationapp/dto/UserRequest.java
+++ src/main/java/com/example/userregistrationapp/dto/UserRequest.java
@@ -1,18 +1,28 @@
 package com.example.userregistrationapp.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.aspectj.bridge.IMessage;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class UserRequest {
-    private Long id;
+
+    @NotBlank(message = "first name must not be null")
+    @Size(min = 3, message = "first name must not be minimum of 3 characters")
     private String firstName;
+    @NotBlank(message = "last name must not be empty")
+    @Size(min = 3, message = "lastname must not be minimum of 3 characters")
     private String lastName;
+    @NotBlank(message = "email must be valid")
     private String email;
+
     private String phoneNumber;
     private String password;
 
--- src/main/java/com/example/userregistrationapp/exception/exceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/example/userregistrationapp/exception/exceptionHandler/GlobalExceptionHandler.java
@@ -8,8 +8,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RequiredArgsConstructor
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(UserNotFoundException.class)
