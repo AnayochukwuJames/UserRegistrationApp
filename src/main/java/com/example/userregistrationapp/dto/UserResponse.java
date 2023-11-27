4951abbdaf918a1407eb55e0ffcf0b26f7499915
AnayochukwuJames <anayochukwujames11@gmail.com>
2023-11-27 07:07:29 +0100

    Update initial commit

--- src/main/java/com/example/userregistrationapp/dto/UserResponse.java
+++ src/main/java/com/example/userregistrationapp/dto/UserResponse.java
@@ -1,5 +1,7 @@
 package com.example.userregistrationapp.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -9,7 +11,11 @@ import lombok.*;
 @Builder
 public class UserResponse {
     private Long id;
+    @NotBlank(message = "first name must not be blank" )
+    @Size(min = 3, message = "first name must be minimum of 3 characters")
     private String firstName;
+    @NotBlank(message = "last name must not be blank")
+    @Size(min = 3, message = "last name must be minimum of 3 characters")
     private String lastName;
     private String email;
     private String phoneNumber;
