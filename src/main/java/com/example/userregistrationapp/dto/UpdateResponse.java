98308ac6dc889ad676c5093a899f62d478b6ba99
AnayochukwuJames <anayochukwujames11@gmail.com>
2023-12-29 10:45:57 +0100

    Update initial commit

--- src/main/java/com/example/userregistrationapp/dto/UpdateResponse.java
+++ src/main/java/com/example/userregistrationapp/dto/UpdateResponse.java
@@ -8,26 +8,16 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
-
 @Setter
-
 @AllArgsConstructor
-
 @NoArgsConstructor
-
 public class UpdateResponse {
-
     @NotBlank(message = "first name must not be null")
-
     @Size(min = 3, message = "first name must not be minimum of 3 characters")
-
     private String firstName;
     @NotBlank(message = "last name must not be blank")
-
     @Size(min = 3, message = "last name must be minimum of 3 characters")
-
     private String lastName;
     @NotBlank(message = "phoneNumber must not be empty")
-
     private String phoneNumber;
 }
